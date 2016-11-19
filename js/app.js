4dc40e4df6fc8379e4c725305dd3a643b4d67c0d
angel <angel.avila.13@hotmail.com>
2016-11-19 00:05:02 -0600

    funcionalidad basica agregada

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
+// Vue.config.debug = true;
 var app = new Vue({
-    el : '#app',
+    el: '#app',
     data: {
-        userName : '',
-        activeTitle:'',
-        titleDev:'',
-        contentDev:''
+        userName: '',
+        newNote: {
+            title: '',
+            content: '',
+            tags: ''
+        },
+        notes: [{
+            title: '',
+            content: '',
+            tags: '',
+        }],
+    },
+    methods: {
+        empyNewNote: function(){
+            this.newNote = {
+                title: '',
+                content: '',
+                tags: ''
+            };
+        },
+        addNote: function(add) {
+            if (this.newNote.title !== '' && this.newNote.content !== '') {
+                if (add===true) {
+                    this.notes.push(this.newNote);
+                    this.empyNewNote();
+                }else{
+                    this.notes.push(this.newNote);
+                }
+            }
+        },
     }
 });
